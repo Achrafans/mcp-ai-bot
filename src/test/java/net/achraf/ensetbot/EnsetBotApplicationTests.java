57c8bb0ddeab20bab874f67132c81ea64de63ecf
achrafansari <achraf.ansari@centoria.fr>
2025-12-07 15:42:42 +0100

    feat: init project

---
+++ src/test/java/net/achraf/ensetbot/EnsetBotApplicationTests.java
@@ -0,0 +1,13 @@
+package net.achraf.ensetbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnsetBotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
